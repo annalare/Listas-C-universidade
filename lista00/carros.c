ba3c33dc2ba29637df937d033fa459d4dd02da5d
Larissa Mendes <83883968+annalare@users.noreply.github.com>
2022-07-12 11:14:59 -0300

    lista00

---
+++ lista00/carros.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+  int dias,kmrodado;
+scanf("%d",&dias);
+scanf("%d",&kmrodado);
+double preco;
+ double valortotal = (dias*30+kmrodado*0.01)*0.90;
+printf("%f" ,valortotal);
+	return 0;
+}
+ 
---
+++ lista00/laranjas.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+int main(){
+int quantidade;
+scanf("%d",&quantidade);
+double laranja =8.35/12;
+int duzia = (quantidade/12);
+double valor = (quantidade*laranja);
+printf("%d\n""%0.2f",duzia,valor);
+return 0;
+}
+
